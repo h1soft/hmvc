d57658d3f707f797f37b9e9e34e85998332a7c74
h1soft <h@h1soft.net>
2015-12-27 16:53:54 +0800

    0.0.2

--- src/hmvc/Database/Raw.php
+++ src/hmvc/Database/Raw.php
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-/*
- * Copyright (C) 2014 Allen Niu <h@h1soft.net>
-
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-
-
- * This file is part of the hmvc package.
- * (w) http://www.hmvc.cn
- * (c) Allen Niu <h@h1soft.net>
-
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
-
-
- */
-
-namespace hmvc\Database;
-
-/**
- * Description of Raw
- *
- * @author Administrator
- */
-class Raw {
-
-    public $raw;
-
-    public function __construct($rawString) {
-        $this->raw = $rawString;
-    }
-
-    public function __toString() {
-        return $this->raw;
-    }
-
-}
-=======
 <?php
 
 /*
@@ -102,4 +49,3 @@ class Raw {
     }
 
 }
->>>>>>> origin/master
